993ce4bc11d52ec1178c826ab8993e59b2483206
Quoc khang Luong <khanghuu97@gmail.com>
2023-05-01 14:09:40 +0700

    Implement command dispatcher

---
+++ SM-Post/Post.Cmd/Post.Cmd.Infrastructure/Dispatchers/CommandDispatcher.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.Core.Commands;
+using CQRS.Core.Infrastructure;
+
+namespace Post.Cmd.Infrastructure.Dispatchers
+{
+    public class CommandDispatcher : ICommandDispatcher
+    {
+        private Dictionary<Type, Func<BaseCommand, Task>> _handlers { get; set; } = new Dictionary<Type, Func<BaseCommand, Task>>();
+
+        public void RegisterHandler<T>(Func<T, Task> handler) where T : BaseCommand
+        {
+            if(_handlers.ContainsKey(typeof(T)))
+            {
+                throw new IndexOutOfRangeException("Cannot register the same Command handler twice");
+            }
+
+            _handlers.Add(typeof(T), x => handler((T)x));
+        }
+
+        public async Task SendAsync(BaseCommand command)
+        {
+            if(_handlers.TryGetValue(command.GetType(), out Func<BaseCommand, Task> handler))
+            {
+                await handler(command);
+            }
+            else
+            {
+                throw new ArgumentException(nameof(handler), "No Command handler was registerd.");
+            }
+        }
+    }
+}
